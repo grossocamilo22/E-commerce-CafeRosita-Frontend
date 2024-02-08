6217c4be9bb6f00153687131d6f2416f84b219b1
Juan Camilo <grosso.juan@correounivalle.edu.co>
2024-02-08 18:01:18 -0500

    version 2

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-{
-  path:'auth',
-  loadChildren:()=>import('../auth/auth.module').then(m=>m.AuthModule)
-}
+  {
+    path: 'auth',
+    loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '**',//TODO 404 cuando no existe la ruta
+    redirectTo: '/auth'
+  }
 
 ];
 
